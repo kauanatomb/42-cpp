330b63d38419e6c5d36440b3f4fdc8c3bcc65f17
Kauana Tombolato <kauanatomb@gmail.com>
2025-11-29 18:07:51 +0100

    removed typo

--- c03/ex01/src/ScavTrap.cpp
+++ c03/ex01/src/ScavTrap.cpp
@@ -4,7 +4,7 @@ ScavTrap::ScavTrap(const std::string& inputName) : ClapTrap(inputName) {
     hitPoints = 100;
     energyPoints = 50;
     attackDamage = 20;
-    std::cout << "ScarvTrap constructed: " << name << std::endl ;
+    std::cout << "ScavTrap constructed: " << name << std::endl ;
 }
 
 ScavTrap::~ScavTrap() {
