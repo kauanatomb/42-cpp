cb13a368410750f57ec388a7f3cafc34bc850400
Kauana Tombolato <kauanatomb@gmail.com>
2026-01-01 16:38:45 +0100

    done last one

---
+++ c08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+    public:
+        MutantStack() {}
+        MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+        MutantStack& operator=(const MutantStack& other)
+        {
+            std::stack<T>::operator=(other);
+            return *this;
+        }
+        ~MutantStack() {}
+
+        /* Iterator types */
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+        /* Iterator access */
+        iterator begin() { return this->c.begin(); }
+        iterator end()   { return this->c.end(); }
+
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end()   const { return this->c.end(); }
+};
+
+#endif
