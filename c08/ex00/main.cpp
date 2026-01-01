f90b23cdf58c86b926eef5f31dce84904bf2a623
Kauana Tombolato <kauanatomb@gmail.com>
2026-01-01 15:43:28 +0100

    done first exercise

---
+++ c08/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include "easyfind.hpp"
+
+int main() {
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    try
+    {
+        std::vector<int>::iterator it = easyfind(v, 5);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "Not found" << std::endl;
+    }
+}
