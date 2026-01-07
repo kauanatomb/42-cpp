a7c3e532401aa780033861527fac14e8c6db6ae9
kauanatomb <kauanatomb@gmail.com>
2026-01-07 17:10:48 +0100

    done last exercise

--- c09/ex02/PmergeMe.cpp
+++ c09/ex02/PmergeMe.cpp
@@ -11,7 +11,31 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
     return *this;
 }
 
-void PmergeMe::makePairs(std::vector< std::pair<int,int> >& pairs,
+void PmergeMe::evaluate(int ac, char **av) {
+    for (int i = 1; i < ac; ++i) {
+        char *end;
+        long n = std::strtol(av[i], &end, 10);
+        if (*end != '\0' || n < 0 || n > INT_MAX)
+            throw std::runtime_error("Error");
+        vec.push_back(static_cast<int>(n));
+        deq.push_back(static_cast<int>(n));
+    }
+    
+    PmergeMe::printContainer(vec, "Before: ");
+
+    clock_t start_v = clock();
+    sortVector();
+    clock_t end_v = clock();
+    clock_t start_d = clock();
+    sortDeque();
+    clock_t end_d = clock();
+    PmergeMe::printContainer(vec, "After: ");
+    PmergeMe::timerCalcPrint(start_v, end_v, vec.size(), "std::vector");
+    PmergeMe::timerCalcPrint(start_d, end_d, deq.size(), "std::deque");
+}
+
+// vector
+void PmergeMe::makePairsV(std::vector< std::pair<int,int> >& pairs,
     bool& hasStraggler, int& straggler) {
     
     hasStraggler = false;
@@ -32,7 +56,7 @@ void PmergeMe::makePairs(std::vector< std::pair<int,int> >& pairs,
     }
 }
 
-static std::vector<size_t> buildJacobIndices(size_t n) {
+std::vector<size_t> PmergeMe::buildJacobIndicesV(size_t n) {
     std::vector<size_t> res;
     if (n <= 1)
         return res;
@@ -48,11 +72,10 @@ static std::vector<size_t> buildJacobIndices(size_t n) {
     return res;
 }
 
-
-void PmergeMe::insertMinors(const std::vector<int>& minors,
+void PmergeMe::insertMinorsV(const std::vector<int>& minors,
                            std::vector<int>& mainChain)
 {
-    std::vector<size_t> jacob = buildJacobIndices(minors.size());
+    std::vector<size_t> jacob = buildJacobIndicesV(minors.size());
     std::vector<bool> used(minors.size(), false);
 
     for (int i = jacob.size() - 1; i >= 0; --i) {
@@ -80,7 +103,7 @@ void PmergeMe::sortVector() {
     bool hasStraggler;
     int straggler;
 
-    makePairs(pairs, hasStraggler, straggler);
+    makePairsV(pairs, hasStraggler, straggler);
 
     std::vector<int> mainChain;
     std::vector<int> minors;
@@ -91,7 +114,7 @@ void PmergeMe::sortVector() {
 
     std::sort(mainChain.begin(), mainChain.end());
 
-    insertMinors(minors, mainChain);
+    insertMinorsV(minors, mainChain);
 
     if (hasStraggler) {
         std::vector<int>::iterator it = std::lower_bound(mainChain.begin(),
@@ -99,33 +122,101 @@ void PmergeMe::sortVector() {
         mainChain.insert(it, straggler);
     }
 
-    std::cout << "After: ";
-    for (size_t i = 0; i < mainChain.size(); ++i)
-        std::cout << mainChain[i] << " ";
-    std::cout << "\n";
+    vec.clear();
+    for (size_t j = 0; j < mainChain.size(); ++j)
+        vec.push_back(mainChain[j]);
 }
 
-void PmergeMe::evaluate(int ac, char **av) {
-    for (int i = 1; i < ac; ++i) {
-        char *end;
-        long n = std::strtol(av[i], &end, 10);
-        if (*end != '\0' || n < 0 || n > INT_MAX)
-            throw std::runtime_error("Error");
-        vec.push_back(static_cast<int>(n));
-    }
-    if (vec.size() < 2)
-        throw std::runtime_error("Error");
-    
-    std::cout << "Before: ";
-    for (size_t i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << " ";
-    std::cout << "\n";
+// deque
 
-    clock_t start = clock();
-    sortVector();
-    clock_t end = clock();
-    double duration = double(end - start) / CLOCKS_PER_SEC * 1e6;
-    std::cout << "Time to process a range of " << vec.size()
-          << " elements with std::vector : " 
-          << duration << " us" << std::endl;
+void PmergeMe::makePairsD(std::deque< std::pair<int,int> >& pairs,
+    bool& hasStraggler, int& straggler) {
+    
+    hasStraggler = false;
+    size_t i = 0;
+    for (; i + 1 < deq.size(); i += 2) {
+        int a = deq[i];
+        int b = deq[i + 1];
+
+        if (a > b)
+            std::swap(a, b);
+
+        pairs.push_back(std::make_pair(a, b));
+    }
+
+    if (i < deq.size()) {
+        hasStraggler = true;
+        straggler = deq[i];
+    }
+}
+
+std::deque<size_t> PmergeMe::buildJacobIndicesD(size_t n) {
+    std::deque<size_t> res;
+    if (n <= 1)
+        return res;
+
+    size_t a = 1;
+    size_t b = 3;
+    while (a < n) {
+        res.push_back(a);
+        size_t c = b + 2 * a;
+        a = b;
+        b = c;
+    }
+    return res;
+}
+
+void PmergeMe::insertMinorsD(const std::deque<int>& minors,
+                           std::deque<int>& mainChain)
+{
+    std::deque<size_t> jacob = buildJacobIndicesD(minors.size());
+    std::deque<bool> used(minors.size(), false);
+
+    for (int i = jacob.size() - 1; i >= 0; --i) {
+        size_t idx = jacob[i];
+        int val = minors[idx];
+
+        std::deque<int>::iterator it =
+            std::lower_bound(mainChain.begin(), mainChain.end(), val);
+        mainChain.insert(it, val);
+        used[idx] = true;
+    }
+
+    for (int i = minors.size() - 1; i >= 0; --i) {
+        if (used[i])
+            continue;
+        int val = minors[i];
+        std::deque<int>::iterator it =
+            std::lower_bound(mainChain.begin(), mainChain.end(), val);
+        mainChain.insert(it, val);
+    }
+}
+
+void PmergeMe::sortDeque() {
+    std::deque< std::pair<int,int> > pairs;
+    bool hasStraggler;
+    int straggler;
+
+    makePairsD(pairs, hasStraggler, straggler);
+
+    std::deque<int> mainChain;
+    std::deque<int> minors;
+    for (size_t i = 0; i < pairs.size(); ++i) {
+        mainChain.push_back(pairs[i].second);
+        minors.push_back(pairs[i].first); 
+    }
+
+    std::sort(mainChain.begin(), mainChain.end());
+
+    insertMinorsD(minors, mainChain);
+
+    if (hasStraggler) {
+        std::deque<int>::iterator it = std::lower_bound(mainChain.begin(),
+            mainChain.end(), straggler);
+        mainChain.insert(it, straggler);
+    }
+
+    deq.clear();
+    for (size_t j = 0; j < mainChain.size(); ++j)
+        deq.push_back(mainChain[j]);
 }
--- c09/ex02/PmergeMe.hpp
+++ c09/ex02/PmergeMe.hpp
@@ -14,11 +14,36 @@ class PmergeMe {
     private:
         std::vector<int> vec;
         std::deque<int> deq;
+
         void sortVector();
-        void makePairs(std::vector< std::pair<int,int> >& pairs,
+        void makePairsV(std::vector< std::pair<int,int> >& pairs,
             bool& hasStraggler, int& straggler);
-        void insertMinors(const std::vector<int>& minors,
+        void insertMinorsV(const std::vector<int>& minors,
             std::vector<int>& mainChain);
+        std::vector<size_t> buildJacobIndicesV(size_t n);
+
+        void sortDeque();
+        void makePairsD(std::deque< std::pair<int,int> >& pairs,
+            bool& hasStraggler, int& straggler);
+        void insertMinorsD(const std::deque<int>& minors,
+            std::deque<int>& mainChain);
+        std::deque<size_t> buildJacobIndicesD(size_t n);
+
+        template <typename Container>
+        static void printContainer(const Container& c, const char* prefix = "") {
+            if (prefix && prefix[0] != '\0')
+                std::cout << prefix;
+            for (typename Container::const_iterator it = c.begin(); it != c.end(); ++it) {
+                std::cout << *it << " ";
+            }
+            std::cout << "\n";
+        }
+        static void timerCalcPrint(clock_t start, clock_t end, size_t n, const std::string& type) {
+            double duration = double(end - start) / CLOCKS_PER_SEC * 1e6;
+            std::cout << "Time to process a range of " << n
+                    << " elements with " << type << ": "
+                    << duration << " us" << std::endl;
+        }
     public:
         PmergeMe();
         ~PmergeMe();
